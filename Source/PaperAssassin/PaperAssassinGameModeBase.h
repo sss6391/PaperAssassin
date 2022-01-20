a1c9d441cb8447c51031e966a26865ee45fb4098
sss6391 <thsqudcks6@naver.com>
2022-01-21 05:41:32 +0900

    첫 커밋

---
+++ Source/PaperAssassin/PaperAssassinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PaperAssassinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAPERASSASSIN_API APaperAssassinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
